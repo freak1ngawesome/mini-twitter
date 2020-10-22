384dbb4effdc52e5b1cad53775ea809aa4f48e75
freak1ngawesome <qwerty4985@mail.ru>
2020-10-22 11:51:27 +0300

    Updates

---
+++ precache-manifest.9f251e399291992d5d09b03d16394d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5070920a465874193993cb3ca56212",
+    "url": "/mini-twitter/index.html"
+  },
+  {
+    "revision": "3cd4fa3de50868e754d4",
+    "url": "/mini-twitter/static/css/main.7689019f.chunk.css"
+  },
+  {
+    "revision": "d66e189dc2dba82be7cf",
+    "url": "/mini-twitter/static/js/2.01e22d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-twitter/static/js/2.01e22d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd4fa3de50868e754d4",
+    "url": "/mini-twitter/static/js/main.eb1e8d5c.chunk.js"
+  },
+  {
+    "revision": "37abbc9aa29fee56bf13",
+    "url": "/mini-twitter/static/js/runtime-main.71e3a5b4.js"
+  }
+]);
